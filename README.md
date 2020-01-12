b7f69cc41b5185e5374aa2351c96875af44d135b
Eric Jay Stevens <ericjaystevens@gmail.com>
2020-01-11 22:43:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Chocolatey package for managing the CircleCI-cli tool.
+
+## How to use
+
+```powershell
+choco install circleci-cli -y
+```
---
+++ circleci-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';	
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition	
+$url64      = 'https://github.com/CircleCI-Public/circleci-cli/releases/download/v0.1.5879/circleci-cli_0.1.5879_windows_amd64.zip'	
+
+ $packageParams = @{	
+  packageName     = $env:ChocolateyPackageName	
+  unzipLocation   = $toolsDir	
+  url64           = $url64	
+  checksum64    = 'ca7fef41ca01cad6efef5adef0283bc314872ce82ab815c562c610d16e239af0'	
+  checksumType64  = 'sha256'	
+}	
+Install-ChocolateyZipPackage @packageParams
+
