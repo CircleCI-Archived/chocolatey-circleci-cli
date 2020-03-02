e29321ff04f77cb5d8d5730befe67bbbc51ed488
Eric Jay Stevens <ericjaystevens@gmail.com>
2020-03-01 21:25:07 -0500

    updated version

--- circleci-cli/tools/chocolateyinstall.ps1
+++ circleci-cli/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';	
-$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition	
-$url64      = 'https://github.com/CircleCI-Public/circleci-cli/releases/download/v0.1.5879/circleci-cli_0.1.5879_windows_amd64.zip'	
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition	
+$url64 = 'https://github.com/CircleCI-Public/circleci-cli/releases/download/v0.1.6648/circleci-cli_0.1.6648_windows_amd64.zip'	
 
- $packageParams = @{	
-  packageName     = $env:ChocolateyPackageName	
-  unzipLocation   = $toolsDir	
-  url64           = $url64	
-  checksum64    = 'ca7fef41ca01cad6efef5adef0283bc314872ce82ab815c562c610d16e239af0'	
-  checksumType64  = 'sha256'	
+$packageParams = @{	
+  packageName    = $env:ChocolateyPackageName	
+  unzipLocation  = $toolsDir	
+  url64          = $url64	
+  checksum64     = '36aed8f51f3ad8caa991d87a15e1938fed5e2daa612ecc62a842f381f7d87a1d'	
+  checksumType64 = 'sha256'	
 }	
 Install-ChocolateyZipPackage @packageParams
 
