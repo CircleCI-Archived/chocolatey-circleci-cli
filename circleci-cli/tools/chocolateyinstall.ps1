ae9f9e51f17d7818546e0eeda5bd4199011e1a70
Your Name <youremail@example.com>
2020-05-08 22:04:39 -0400

    automated deploy and upgrade to 0.1.7251

--- circleci-cli/tools/chocolateyinstall.ps1
+++ circleci-cli/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';	
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition	
-$url64 = 'https://github.com/CircleCI-Public/circleci-cli/releases/download/v0.1.6648/circleci-cli_0.1.6648_windows_amd64.zip'	
+$url64 = 'https://github.com/CircleCI-Public/circleci-cli/releases/download/v0.1.7251/circleci-cli_0.1.7251_windows_amd64.zip'	
 
 $packageParams = @{	
   packageName    = $env:ChocolateyPackageName	
   unzipLocation  = $toolsDir	
   url64          = $url64	
-  checksum64     = '36aed8f51f3ad8caa991d87a15e1938fed5e2daa612ecc62a842f381f7d87a1d'	
+  checksum64     = 'b801993e5b0f6f0ae640a177d28a78a43af18a138dd3ee3ab1de918551ed265b'	
   checksumType64 = 'sha256'	
 }	
 Install-ChocolateyZipPackage @packageParams
