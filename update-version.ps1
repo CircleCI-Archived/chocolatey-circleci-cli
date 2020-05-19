8aed9af8fa80441ca02ae5cb70d3048e9d00dc23
Eric Stevens <ericjaystevens@gmail.com>
2020-05-19 17:07:19 -0400

    push it

--- update-version.ps1
+++ update-version.ps1
@@ -8,10 +8,11 @@ $versionURL = $response.GetResponseHeader("Location")
 
 $curVersion = ($versionURL | Split-Path -Leaf).Substring(1)
 
-$tmpFile = ".\checksum.txt"
+$tmpFile = ".\out.txt"
 $checksumURL = "https://github.com/CircleCI-Public/circleci-cli/releases/download/v$curVersion/circleci-cli_$($curVersion)_checksums.txt"
 Write-Verbose "getting checksome from $checksumURL to $tmpFile"
-Invoke-WebRequest $checksumURL -OutFile $tmpFile
+
+(New-Object System.Net.WebClient).DownloadString($checksumURL) >> $tmpFile
 
 ### replace hash
 $hash = (Get-Content $tmpFile | where {$_ -like "*windows_amd64.zip"}).split(" ")[0]
@@ -21,5 +22,5 @@ $installerPath = ".\circleci-cli\tools\chocolateyinstall.ps1"
 $downloadURL = "https://github.com/CircleCI-Public/circleci-cli/releases/download/v$curVersion/circleci-cli_$($curVersion)_windows_amd64.zip"
 (Get-Content $installerPath).Replace('$DOWNLOAD_URL',$downloadURL) | Out-File $installerPath -Force
 
-$nuspecPath = "circleci-cli.nuspec"
+$nuspecPath = "./circleci-cli/circleci-cli.nuspec"
 (Get-Content $nuspecPath).Replace('$VER',$curVersion) | Out-File $nuspecPath -Force
